d76fd8ae95ba438872f3a20fe0c686db209a7764
Minhsun88 <4a7k0032@stust.edu.tw>
2022-07-29 14:56:30 +0800

    feat:後端會員管理系統

---
+++ app/controller/manager.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class managerController extends Controller {
+  async login() {
+    const { ctx } = this;
+    await ctx.render('login.ejs');
+  } 
+  async identify() {
+    const { ctx } = this;
+    const params = this.ctx.request.body;
+    let result = await this.ctx.service.manager.getData(params);
+    if(result === true) {
+      await ctx.redirect('/member/1');
+    } else {
+      await ctx.render('login.ejs', { msg: result.msg, account:result.account, password: result.password });
+    } 
+  } 
+} 
+
+module.exports = managerController;
