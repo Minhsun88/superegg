55db1e55f4a1f9ff11cfaac10600aedc8b9ff55a
Minhsun88 <4a7k0032@stust.edu.tw>
2022-07-29 15:19:37 +0800

    12344

--- app/controller/manager.js
+++ app/controller/manager.js
@@ -10,8 +10,8 @@ class managerController extends Controller {
   async identify() {
     const { ctx } = this;
     const params = this.ctx.request.body;
-    let certificate = await this.ctx.service.manager.getData(params);
-    if(certificate === true) {
+    const certificate = await this.ctx.service.manager.getData(params);
+    if(certificate === true){
       await ctx.redirect('/member/1');
     }else {
       await ctx.render('login.ejs', { msg: certificate.msg, account: certificate.account, password: certificate.password });
